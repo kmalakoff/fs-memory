47fde602eabe4943dd105b1bd1d3068283bba918
Kevin Malakoff <kmalakoff@gmail.com>
2015-11-11 08:16:04 +0000

    Bug fix

--- src/instance-methods/mkdirSync.js
+++ src/instance-methods/mkdirSync.js
@@ -1,24 +1,12 @@
 import {sep} from 'path';
-
-import {findNextNode} from '../lib/utils';
+import {findNode, findOrCreateChild} from '../lib/utils';
 import FSError from '../lib/error';
 import ERRORS from 'errno';
 
 export default function mkdirSync(path) {
-  var pathParts = path.split(path, sep);
-  if (!pathParts.length) return;
-  let current, next = this.rootNode;
-  let name;
-
-  for (let i = 0; i < pathParts.length - 1; i++) {
-    name = pathParts[i];
-    next = findNextNode(current, name);
-    if (!next) {
-      if (!isName(name)) throw new FSError(ERRORS.code.ENOENT, path);
-      else next = new Node({name, isDirectory: true})
-      current = next;
-    }
-    else if (next.data.isDirectory) throw new FSError(ERRORS.code.EEXIST, path);
-    else throw new FSError(ERRORS.code.ENOTDIR, path);
-  }
+  var parts = path.split(sep), name = parts.pop(), parentPath = parts.join(sep);
+  let parentNode = findNode(this.rootNode, parentPath);
+  if (!parentNode) throw new FSError(ERRORS.code.ENOENT, parentPath);
+  var node = findOrCreateChild(parentNode, {isDirectory: true, name});
+  if (!node.data.isDirectory) throw new FSError(ERRORS.code.ENOTDIR, path);
 }
