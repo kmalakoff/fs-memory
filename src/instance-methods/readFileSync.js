1242444411fa1610ab0a3e760f7af4b5f1f86a36
Kevin Malakoff <kmalakoff@gmail.com>
2015-12-20 17:20:18 -0800

    Improved encoding checks

--- src/instance-methods/readFileSync.js
+++ src/instance-methods/readFileSync.js
@@ -4,6 +4,7 @@ import FSError from '../lib/error';
 import {findNode} from '../lib/utils';
 import {nodeIsDirectory} from '../lib/mode';
 import _ from 'lodash';
+import encodings from '../lib/encodings';
 
 export default function readFileSync(path, options) {
   let node = findNode(this.rootNode, path);
@@ -11,7 +12,6 @@ export default function readFileSync(path, options) {
   if (nodeIsDirectory(node)) throw new FSError(ERRORS.code.EISDIR, path);
 
   let encoding = !options || options.encoding || options;
-  if (!_.isString(encoding)) encoding = undefined;
-
-  return encoding ? bodec.toString(node.data.contents, encoding) : node.data.contents;
+  let stringEncoding = _.isString(encoding) && ~encodings.string.indexOf(encoding) ? encoding : undefined;
+  return stringEncoding ? bodec.toString(node.data.contents, stringEncoding) : node.data.contents;
 }
--- src/instance-methods/writeFileSync.js
+++ src/instance-methods/writeFileSync.js
@@ -7,8 +7,9 @@ import FSError from '../lib/error';
 import {findNode, findChild} from '../lib/utils';
 import {Node} from 'forestry';
 import {nodeIsDirectory} from '../lib/mode';
+import encodings from '../lib/encodings';
 
-export default function writeFileSync(path, data, encoding) {
+export default function writeFileSync(path, data, options) {
   var parts = path.split(sep), name = parts.pop(), parentPath = parts.join(sep);
   let parentNode = findNode(this.rootNode, parentPath);
   if (!parentNode) throw new FSError(ERRORS.code.ENOENT, parentPath);
@@ -23,7 +24,10 @@ export default function writeFileSync(path, data, encoding) {
     node = new Node({name, stat: {mode: 0o100000, size: 0, ctime: now, mtime: now, birthtime: now}});
     parentNode.addChild(node);
   }
-  node.data.contents = encoding ? bodec.fromString(data, encoding) : bodec.copy(data);
+
+  let encoding = !options || options.encoding || options;
+  let stringEncoding = _.isString(encoding) && ~encodings.string.indexOf(encoding) ? encoding : undefined;
+  node.data.contents = stringEncoding ? bodec.fromString(data, stringEncoding) : bodec.copy(data);
   node.data.stat = _.defaults({size: node.data.contents.length, mtime: now}, node.data.stat || {})
   EventHandlers.emit(exists ? 'update' : 'add', path);
 }
